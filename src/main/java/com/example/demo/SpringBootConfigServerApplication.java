07734e6e72095d1164e5c23a382149fc5463a2f1
saurabhmca_2004 <saurabhmca_2004@saurabhpc>
2020-05-03 14:06:14 +0530

    My 1st commit.

--- src/main/java/com/example/demo/SpringBootConfigServerApplication.java
+++ src/main/java/com/example/demo/SpringBootConfigServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class SpringBootConfigServerApplication {
-
+//git commit.
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootConfigServerApplication.class, args);
 	}
